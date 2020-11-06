eb1c1aab97e6b749a21f45efab4d6f8a4b20ea29
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-06 11:49:26 -0800

    remove default files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render((
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/quizzes" component={Quizzes} />
+      <Route exact path="/quiz/new" component={QuizForm} />
+    </div>
+  </Router>),
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
