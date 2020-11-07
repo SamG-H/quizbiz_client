f1eb0084cddc826b82bf4af8e41e5469eaec3974
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-07 13:56:18 -0800

    add quizzes container component

---
+++ src/components/QuizzesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class QuizzesContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
