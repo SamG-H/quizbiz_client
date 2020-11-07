d91181798098c94ed6501e260f8afda4f343502e
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-07 13:37:28 -0800

    add redux, thunk, and devtools

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/App'
 import QuizForm from './components/QuizForm'
 import Quiz from './components/Quiz'
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
 ReactDOM.render((
+  <Provider store={store}>
   <Router>
     <div>
       <Route exact path="/" component={App} />
       <Route exact path="/quizzes" component={Quiz} />
       <Route exact path="/quiz/new" component={QuizForm} />
     </div>
-  </Router>),
+  </Router>
+  </Provider>),
   document.getElementById('root')
 );
